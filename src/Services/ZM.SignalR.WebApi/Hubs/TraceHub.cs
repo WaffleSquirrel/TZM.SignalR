eca554faeda90a81d20e236f47696f3a0d2a4a0e
WaffleSquirrel <rockabilly@outlook.com>
2015-09-09 23:15:56 -0700

    Adding references to the SignalR and Owin'y things. Adding a TraceHub class to act as the SignalR Hub for interception of WebApi tracing.

---
+++ src/Services/ZM.SignalR.WebApi/Hubs/TraceHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ZM.SignalR.WebApi.Hubs
+{
+    [HubName("trace")]
+    public class TraceHub : Hub
+    {
+    }
+}
