b0f6e34b82b287064ef939327d2536a33290d6c0
WaffleSquirrel <rockabilly@outlook.com>
2015-09-11 23:09:41 -0700

    Adding an Infrastructure.Communicatons namespace/folder and moved the Hubs namespace/folder to it.

--- src/ZM.SignalR.Integrations.WebApiMvc/Infrastructure/Communication/Hubs/TraceHub.cs
+++ src/ZM.SignalR.Integrations.WebApiMvc/Infrastructure/Communication/Hubs/TraceHub.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 
-namespace ZM.SignalR.Integrations.WebApiMvc.Hubs
+namespace ZM.SignalR.Integrations.WebApiMvc.Infrastructure.Communication.Hubs
 {
     [HubName("trace")]
     public class TraceHub : Hub
