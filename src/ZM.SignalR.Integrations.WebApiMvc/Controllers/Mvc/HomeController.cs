15c4a0b23c31918dbf86effc736ec460279003b6
WaffleSquirrel <rockabilly@outlook.com>
2015-09-11 23:51:32 -0700

    Updating Home controller to derive from the base MVC Hub controller.

---
+++ src/ZM.SignalR.Integrations.WebApiMvc/Controllers/Mvc/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using ZM.SignalR.Integrations.WebApiMvc.Infrastructure.Communication.Hubs;
+using ZM.SignalR.Integrations.WebApiMvc.Infrastructure.Mvc;
+
+namespace ZM.SignalR.Integrations.WebApiMvc.Controllers.Mvc
+{
+    /// <summary>
+    /// The home MVC controller for the Web API UI.
+    /// </summary>
+    public class HomeController : BaseMvcHubController<ConnectionBroadcaster>
+    {
+        /// <summary>
+        /// Returns the default view for displaying the Home Page.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page - ZM SignalR Web Integrations";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Returns the default view for displaying the Human Page.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Human()
+        {
+            return View();
+        }
+    }
+}
