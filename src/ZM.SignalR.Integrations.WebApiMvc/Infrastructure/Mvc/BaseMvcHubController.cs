ff625078f428fadd1be824a2064ff6619a97fb54
WaffleSquirrel <rockabilly@outlook.com>
2015-09-11 23:30:55 -0700

    Adding a base MVC and Web API controller that contains common definition  of properties that are used for managing Hub connections.

---
+++ src/ZM.SignalR.Integrations.WebApiMvc/Infrastructure/Mvc/BaseMvcHubController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ZM.SignalR.Integrations.WebApiMvc.Infrastructure.Mvc
+{
+    public abstract class BaseMvcHubController<THub> : Controller where THub : Hub
+    {
+        public IHubConnectionContext<dynamic> Clients { get; private set; }
+        public IGroupManager Groups { get; private set; }
+
+        protected BaseMvcHubController()
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<THub>();
+
+            this.Clients = hubContext.Clients;
+            this.Groups = hubContext.Groups;
+        }
+    }
+}
